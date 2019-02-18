28320b02ca0cb5f11f0807063bf04fa0dcced4e6
endesigner <igor@github.endesigner.se>
2019-02-18 14:00:32 +0100

    Test stub

--- src/test/java/igor/APIDispatcherTest.java
+++ src/test/java/igor/APIDispatcherTest.java
@@ -3,48 +3,21 @@ package igor;
 import io.vertx.core.Vertx;
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.TestSuite;
 import io.vertx.ext.web.client.WebClient;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
+// TODO write async test https://vertx.io/docs/vertx-unit/java/
 public class APIDispatcherTest {
   private Vertx vertx;
 
-  @Before
-  public void setup(TestContext testContext) {
-    vertx = Vertx.vertx();
-
-    vertx.deployVerticle(
-      APIDispatcher.class.getName(),
-      testContext.asyncAssertSuccess()
-    );
-  }
-
-  @After
-  public void tearDown(TestContext testContext) {
-    vertx.close(testContext.asyncAssertSuccess());
-  }
-
   @Test
-  public void whenReceivedResponse_thenSuccess(TestContext testContext) {
-    Async async = testContext.async();
-    WebClient client = WebClient.create(vertx);
-    client.get(9090, "localhost", "/service")
-      .send(response -> {
-        System.out.println(response.result().body());
-        testContext.assertTrue(response.toString().contains("Hello"));
-        async.complete();
-      });
-
-//    int port = 9090;
-//    vertx.createHttpClient()
-//      .get(port, "localhost", "/service").
-//      , response -> {
-//        response.handler(responseBody -> {
-//          testContext.assertTrue(responseBody.toString().contains("Hello"));
-//          async.complete();
-//        });
-//      });
+  public void runTest() {
+    TestSuite suite = TestSuite.create("the_test_suite");
+    suite.test("my_test_case", context -> {
+      String s = "value";
+      context.assertEquals("value", s);
+    });
+    suite.run();
   }
 }
