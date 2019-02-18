549a73dad741f6ba295d3507ea472516bbef4380
endesigner <igor@github.endesigner.se>
2019-02-18 14:31:47 +0100

    Update test stub

--- src/test/java/igor/APIDispatcherTest.java
+++ src/test/java/igor/APIDispatcherTest.java
@@ -1,10 +1,12 @@
 package igor;
 
 import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpClient;
+import io.vertx.core.http.HttpClientRequest;
 import io.vertx.ext.unit.Async;
-import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.TestOptions;
 import io.vertx.ext.unit.TestSuite;
-import io.vertx.ext.web.client.WebClient;
+import io.vertx.ext.unit.report.ReportOptions;
 import org.junit.Test;
 
 // TODO write async test https://vertx.io/docs/vertx-unit/java/
@@ -13,11 +15,22 @@ public class APIDispatcherTest {
 
   @Test
   public void runTest() {
-    TestSuite suite = TestSuite.create("the_test_suite");
+    vertx = Vertx.vertx();
+    TestSuite suite = TestSuite.create("Test endpoints");
+
     suite.test("my_test_case", context -> {
-      String s = "value";
-      context.assertEquals("value", s);
+      Async async = context.async();
+      HttpClient client = vertx.createHttpClient();
+      HttpClientRequest req = client.get(9090, "localhost", "/");
+      req.exceptionHandler(err -> context.fail(err.getMessage()));
+      req.handler(resp -> {
+        System.out.println("HI@@@@@@@");
+        context.assertEquals(200, resp.statusCode());
+        async.complete();
+      });
+      req.end();
     });
-    suite.run();
+
+    suite.run(new TestOptions().addReporter(new ReportOptions().setTo("console")));
   }
 }
